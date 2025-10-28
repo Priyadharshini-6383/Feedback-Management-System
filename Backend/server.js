b641a15f6236b90fdff754f6811a018bc6e696fb
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-10-28 19:53:15 +0530

    Setting up the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+
+
+
+dotenv.config();
+
 
 const app = express();
 app.use(cors());
 
 
 
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , () => {
+    console.log(`Server running in the PORT http://localhost${PORT}`);
+})
