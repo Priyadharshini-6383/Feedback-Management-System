b4ec701953899c75a412cb7c1e4dc75b52273a68
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-10-30 15:28:17 +0530

    Creating Feedback Routes

--- Backend/routes/feedbackRoutes.js
+++ Backend/routes/feedbackRoutes.js
@@ -1,2 +1,7 @@
 import jsonwebtoken from "jsonwebtoken";
+import express from "express";
+import Feedback from "../models/Feedback.js"
 import { verifyToken , verifyAdmin } from "../middleware/authMiddleware";
+
+const router = express.Router();
+
