962b658c7177978389ec0900d7ee49180e57c5f9
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-10-31 16:21:49 +0530

    Created AuthProvider as a seperate file and implemented the code

---
+++ Frontend/vite-project/src/context/AuthProvider.jsx
@@ -0,0 +1,50 @@
+
+import { useState , useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+
+
+export const AuthProvider = ({children}) => {
+
+    const [user,setUser] = useState(() => {
+        const raw = localStorage.getItem("user");
+       return raw ? JSON.parse(raw) : "null"  
+    })
+
+const [token , setToken] = useState (() =>
+localStorage.getItem("token") || "");
+
+
+  useEffect(() => {
+    if(token && ! user) {
+        const raw = localStorage.getItem("user");
+
+        if(raw)  setUser(JSON.parse(raw)) 
+    }
+} , [user , token]);
+
+ const login = (userData , userTokens) => {
+setUser(userData);
+setToken(userTokens);
+
+localStorage.setItem("user" , JSON.stringify(userData));
+localStorage.setItem("token" , userTokens);
+
+}
+
+ const  logout =  () => {
+    setUser("");
+    setToken("");
+
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+}
+
+return (
+    <AuthContext.Provider value = {{user , token , login , logout}}>
+        {children}
+    </AuthContext.Provider>
+)
+
+
+
+}
