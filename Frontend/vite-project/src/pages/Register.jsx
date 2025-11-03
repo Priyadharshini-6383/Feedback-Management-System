d8388d2c8e8325dd786f79de29c4ae167953bc6c
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-11-03 11:49:32 +0530

    Completed registeration code

--- Frontend/vite-project/src/pages/Register.jsx
+++ Frontend/vite-project/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-//import api from "./api/axios.jsx"
+import api from "./api/axios.jsx"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Register = () => {
@@ -10,45 +10,76 @@ const Register = () => {
         password : ""
     });
 
+    const [Message , setMessage] = useState("");
+
+const handleSubmit = async (e) => {
+      e.preventDefault();
+
+
+      try {
+const res = await api.post("/auth/register" , setData);
+Message(res.data.message);
+setMessage({name : "" , email : "" , password : ""});
+      }
+      catch(error) {
+setMessage("Registeration Failed" , error.message);
+      }
+
+    }
+  
+
 
   return (
+    <div className="container d-flex justify-content-center align-items-center min-vh-100 bg-light">
+  <div className="card p-4 shadow" style={{ width: "400px", borderRadius: "15px" }}>
+    <h1 className="text-center text-primary mb-4">Register</h1>
+
     <div>
-        <h1>Register</h1>
-        <div> 
-
-            <form>
-                <label>Name</label>
-                <input
-                type='text'
-                name = 'name'
-                value = {data.name}
-                placeholder='Enter your name'
-                onChange={(e) => setData(e.target.value)}
-                />
-
-                <label>Email</label>
-                <input
-                type = 'text'
-                name = 'email'
-                value = {data.email}
-                placeholder='Enter you email'
-                onChange={(e) => setData(e.target.value)}
-                
-                />
-                <label>Password</label> 
-                <input 
-                type = 'text'
-                name = 'password'
-                value = {data.password}
-                placeholder='Enter your password'
-                onChange={(e) => setData(e.target.value)}
-                
-                />
-
-                <button>Submit</button>
-            </form>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label fw-semibold">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            placeholder="Enter your name"
+            className="form-control"
+            onChange={(e) => setData({ ...data ,[e.target.name]: e.target.value})}
+          />
         </div>
+
+        <div className="mb-3">
+          <label className="form-label fw-semibold">Email</label>
+          <input
+            type="text"
+            name="email"
+            value={data.email}
+            placeholder="Enter your email"
+            className="form-control"
+            onChange={(e) => setData({ ...data ,[e.target.name]: e.target.value})}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="form-label fw-semibold">Password</label>
+          <input
+            type="text"
+            name="password"
+            value={data.password}
+            placeholder="Enter your password"
+            className="form-control"
+            onChange={(e) => setData({ ...data ,[e.target.name]: e.target.value})}
+          />
+        </div>
+
+        <button className="btn btn-primary w-100 fw-semibold">Submit</button>
+      </form>
+      {Message && (
+  <div className="alert alert-info text-center mt-3">{Message}</div>
+)}
     </div>
+  </div>
+</div>
   )
 }
 
